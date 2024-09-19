37daf3eb80910e999a91602948f18375a82b31da
MiAzami <miazami506@gmail.com>
2024-09-20 02:41:15 +0700

    Update service.sh
    Patched FPSGO & GED

--- service.sh
+++ service.sh
@@ -52,6 +52,54 @@ sync
 # Change zram
 #change_zram
 
+# GED Hal ( Kernel) 
+chmod 644 /sys/kernel/fpsgo/fstb/*
+for fbt in /sys/kernel/fpsgo/fstb
+    do
+        echo 95 > "$fbt/boost_ta"
+        echo 0 > "$fbt/enable_switch_sync_flag"
+    done
+    chmod 444 /sys/kernel/fpsgo/fstb/*
+    
+    
+# GED Hal ( Kernel) 
+for gedh in /sys/kernel/ged/hal
+    do
+        echo 95 > "$gedh/gpu_boost_level"
+        echo 8 > "$gedh/loading_base_dvfs_step"
+    done
+
+# GED Parameter (Module) 
+for gedp in /sys/module/ged/parameters
+    do
+        echo 120 > "$gedp/ged_smart_boost"
+        echo 1 > "$gedp/enable_gpu_boost"
+        echo 1 > "$gedp/ged_boost_enable"
+        echo 1 > "$gedp/boost_gpu_enable"
+        echo 1 > "$gedp/gpu_dvfs_enable"
+        echo 95 > "$gedp/gx_fb_dvfs_margin"
+        echo 100 > "$gedp/gpu_idle"
+        echo 0 > "$gedp/is_GED_KPI_enabled"
+	done
+
+# FPSGo (PNPMGR) 
+for pnp in /sys/pnpmgr
+    do
+        echo 1 > "$pnp/fpsgo_boost/boost_mode"
+        echo 1 > "$pnp/install"
+        echo 1 > "$pnp/mwn"
+        echo 100 > "$pnp/fpsgo_boost/fstb/fstb_tune_quantile"
+        echo 120 > "$pnp/fpsgo_boost/fstb/fstb_fix_fps"
+    done
+    
+# MTKFPS GO Parameter
+for fpsp in /sys/module/mtk_fpsgo/parameters
+    do
+        echo 120 > "$fpsp/boost_affinity"
+        echo 120 > "$fpsp/boost_LR"
+        echo 120 > "$fpsp/xgf_uboost"
+    done
+    
 echo "1" > /sys/module/mtk_core_ctl/parameters/policy_enable
 echo N > /sys/module/sync/parameters/fsync_enabled
 
