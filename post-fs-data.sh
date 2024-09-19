f00c2d81e7fbece0fe2480c4416fe528d2a326f8
MiAzami <miazami506@gmail.com>
2024-09-20 03:39:55 +0700

    Update Busybox
    (3/4)

--- post-fs-data.sh
+++ post-fs-data.sh
@@ -4,24 +4,43 @@
 # Based on Brutal Busybox script.
 MODDIR=${0%/*}
 
-if ! [ -f "$MODDIR/installed" ]; then
-	if [ -d "$MODDIR/system/bin" ]; then
-		chown 0:0 $MODDIR/system/bin/busybox
-		chmod 775 $MODDIR/system/bin/busybox
-		chcon u:object_r:system_file:s0 $MODDIR/system/bin/busybox
-		$MODDIR/system/bin/busybox --install -s $MODDIR/system/bin/
-		for sd in /system/bin/*; do
-			rm -f $MODDIR/${sd}
-		done
-	fi
-	if [ -d "$MODDIR/system/xbin" ]; then
-		chown 0:0 $MODDIR/system/xbin/busybox
-		chmod 775 $MODDIR/system/xbin/busybox
-		chcon u:object_r:system_file:s0 $MODDIR/system/xbin/busybox
-		$MODDIR/system/xbin/busybox --install -s $MODDIR/system/xbin/
-	fi
-	touch $MODDIR/installed
+# Busybox functions
+install_busybox()
+{
+if [ ! -e $MODDIR/system/bin/busybox ]; then
+  cp -f /data/adb/magisk/busybox $MODDIR/system/bin
+  chown 0:0 $MODDIR/system/bin/busybox
+  chmod 775 $MODDIR/system/bin/busybox
+  chcon u:object_r:system_file:s0 $MODDIR/system/bin/busybox
+  $MODDIR/system/bin/busybox --install -s $MODDIR/system/bin/
+  for sd in /system/bin/*; do
+     rm -f $MODDIR/${sd}
+  done
 fi
+
+# Print Busybox Version
+BB_VER="$($a/busybox | head -n1 | cut -f1 -d'(')"
+
+# Install into /system/bin, if exists.
+if [ ! -e /system/xbin ]; then
+	mkdir -p $MODPATH/system/bin
+	mv -f $a/busybox $MODPATH/system/bin/busybox
+	rm -Rf $a
+	
+fi
+}
+
+uninstall_busybox()
+{
+    rm -rf $MODDIR/system/xbin/busybox-arm64
+    rm -rf $MODDIR/system/xbin/busybox
+}
+
+
+# Install built-in magisk busybox
+#install_busybox
+
+#uninstall_busybox
 
 # Deepsleep functions
 doze_disable()
